226f639ff84d10669f078709941a5ace5625aadf
minm15 <r12922153@csie.ntu.edu.tw>
2025-08-28 18:39:25 +0800

    add syscall 397/398 for arm

--- src/arch/arm/linux/se_workload.cc
+++ src/arch/arm/linux/se_workload.cc
@@ -488,7 +488,10 @@ class SyscallTable32 : public SyscallDescTable<EmuLinux::SyscallABI32>
         { base + 363, "sys_rt_tgsigqueueinfo" },
         { base + 364, "sys_perf_event_open" },
         { base + 365, "sys_recvmmsg" },
-        { base + 384, "getrandom", getrandomFunc<ArmLinux32> }
+        { base + 384, "getrandom", getrandomFunc<ArmLinux32> },
+        { base + 397, "sys_statx", ignoreFunc },
+        { base + 398, "sys_rseq", ignoreFunc },
+        // { base + 435, "clone3", clone3Func<ArmLinux32> },
     })
     {}
 };
@@ -782,6 +785,7 @@ class SyscallTable64 : public SyscallDescTable<EmuLinux::SyscallABI64>
         {  base + 292, "io_pgetevents"},
         {  base + 293, "rseq", ignoreWarnOnceFunc },
         {  base + 294, "kexec_file_load"},
+        // {  base + 435, "clone3", clone3Func<ArmLinux64> },
         { base + 1024, "open", openFunc<ArmLinux64> },
         { base + 1025, "link" },
         { base + 1026, "unlink", unlinkFunc },
--- tests/test-progs/CDNCcim/BuildTest.sh
+++ tests/test-progs/CDNCcim/BuildTest.sh
@@ -5,11 +5,3 @@ arm-linux-gnueabihf-g++ -std=gnu++17 -O2 -static \
   -I cim_and/include \
   cim_and/src/simple_test.c \
   -o cim_and/bin/arm/linux/simple_test
-
-
-# arm-linux-musleabihf-g++ -std=gnu++17 -O1 -static -no-pie \
-#  -mtune=cortex-a9 \
-#  -I cim_and/include \
-#   cim_and/src/cim_and.cc \
-#   cim_and/src/cim_api.cpp \
-#   -o cim_and/bin/arm/linux/cim_and
